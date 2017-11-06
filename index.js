d28958c3f706100de2545aacd6ce7c3a9c2bb21e
Michael Gibbons <michaelgibs123@gmail.com>
2017-11-06 11:33:22 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var server_port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/genre', function(req,res){
+    res.sendFile(__dirname + '/views/genre.html');
+});
+app.get('/decade', function(req,res) {
+    res.sendFile(__dirname + '/views/decade.html');
+});
+app.listen(server_port, function() {
+    console.log("Listening on port : " + server_port);
+});
