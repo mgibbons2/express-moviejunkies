99627077c53dd9a10c6240648334023ee7050c8f
Michael <michaelgibs123@gmail.com>
2017-11-12 12:09:33 -0600

    added mongoose, passport

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+
+// getting-started.js
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
+mongoose.Promise = global.Promise;
+var UserSchema = new mongoose.Schema({
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    passwordConf: {
+      type: String,
+      required: true,
+    }
+  });
+  var User = mongoose.model('User', UserSchema);
+  module.exports = User;
+
+
 var server_port = 3000;
 
 app.use(express.static(__dirname + '/public'));
